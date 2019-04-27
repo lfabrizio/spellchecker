019dc7daaef6aa98f12a526d6fa7dc487c6d603d
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-04-27 17:27:21 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,3 @@
+const fruits = ["apple", "bananna", "orange"];
+
+module.exports = fruits;
---
+++ server.js
@@ -0,0 +1,15 @@
+// console.log("server running");
+
+const express = require('express')
+const fruits = require('./data');
+const app = express();
+const port = process.env.PORT || 5000;
+
+console.log('fruits');
+
+app.get('/', function (req, res) {
+    const userName = req.query.user;
+res.send('Hello' + userName + "!")
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
