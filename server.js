37ad36387ca9d1089508f33fa1fd87d669c964a4
lfabrizio <48811036+lfabrizio@users.noreply.github.com>
2019-05-07 09:26:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-// console.log("server running"); comment
+const express = require('express');
+const data = require('./data.js');
+const app = express(); // function call of express..
+const port = process.env.PORT || 8080;; // process.env.PORT || 5000;  
+// if running file locally it will not have process.evn.PORT but runs 5000 instead. Testing.....
 
-const express = require('express')
-const fruits = require('./data');
-const app = express();
-const port = process.env.PORT || 5000;
 
-console.log('fruits');
+app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-    const userName = req.query.user;
-res.send('Hello' + userName + "!")
+app.get("/data", (req, res) => {
+    const word = req.query.word;
+    if (data.hasOwnProperty(word)) {
+        res.send(`The word: ${word} is in here`);
+    } else {
+        res.send(`word ${word} does not exist`);
+    }
+    
+
+
 });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
