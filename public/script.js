ebb3aaa82d16b546ed2f0c21df0e91b066e0f31b
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-05-07 00:49:50 +0000

    updated

---
+++ public/script.js
@@ -0,0 +1,13 @@
+function handleClick(){
+ const word = document.getElementById("word").value;
+
+ 
+ console.log(word);
+  fetch("/data" + "?" + "word=" + word)
+  .then(response=>response.text())
+  .then(function(response){
+   console.log(response);
+   document.getElementById("results").innerHTML = response;
+  });
+
+}
