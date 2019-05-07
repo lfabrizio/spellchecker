15cf9e5711444b2200a8abe847035c7f91d1d6a2
lfabrizio <48811036+lfabrizio@users.noreply.github.com>
2019-05-07 09:27:15 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,11 @@
-const fruits = ["apple", "bananna", "orange"];
+// const fruits= ["apple", "banana", "orange"];
 
-module.exports = fruits;
+// module.exports = fruits;
+
+let users = [];
+users.push({name: "Jason", email:"jason@gmail.com"});
+users.push({name: "Jack", email:"jack@gmail.com"});
+users.push({name: "Sarah", email:"sarah@gmail.com"});
+users.push({name: "Lily", email:"lily@gmail.com"});
+module.exports = users; 
+
